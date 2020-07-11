9c81a634fa140136c0c3ca10ab069a54cf2a6412
sangam bhattarai <bsangam4@gmail.com>
2020-07-11 23:43:10 +0545

    added routes for api endpoints

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('companies', 'CompanyController@index');
+Route::get('companies/{id}', 'CompanyController@show');
+Route::post('companiess', 'CompanyController@store');
+Route::put('companies/{id}', 'CompanyController@update');
+Route::delete('companies/{id}', 'CompanyController@delete');
